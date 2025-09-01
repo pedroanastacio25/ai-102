44a7a7d77a446645b8d1bd27fe2515f07493f1b5
pedro_anastacio <67824386+pedroanastacio25@users.noreply.github.com>
2025-09-01 10:06:42 +0100

    Create doc_read.py

---
+++ document_intelligence/doc_read.py
@@ -0,0 +1,21 @@
+from azure.ai.documentintelligence import DocumentIntelligenceClient
+from azure.ai.documentintelligence.models import AnalyzeDocumentRequest
+from azure.ai.documentintelligence.models import AnalyzeResult
+from azure.core.credentials import AzureKeyCredential
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import utils
+
+endpoint=utils.AZURE_DOC_INTEL_ENDPOINT
+key=utils.AZURE_DOC_INTEL_KEY
+document_url=utils.AZURE_DOC_INTEL_DOC_URL
+
+client=DocumentIntelligenceClient(endpoint=endpoint, credential=AzureKeyCredential(key))
+
+response=client.begin_analyze_document("prebuilt-read", AnalyzeDocumentRequest(url_source=document_url))
+
+result: AnalyzeResult = response.result()
+
+for index,para in enumerate(result.paragraphs):
+    print(f"Paragraph {index+1}: {para.content}")
